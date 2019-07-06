f6b7b3c9e10548227131eeb3ed26978163e921c5
Diego Delgado <diegodelgadoalejandro@gmail.com>
2019-07-06 16:51:21 -0500

    Recipe website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//services
+import { RecipeService } from './services/recipe.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditRecipeComponent,
+    NewRecipeComponent,
+    ShowRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    RecipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
